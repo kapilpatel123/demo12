6c753a61eb9d0f4524998841bb256e0897eb814b
Sunil Kumar <39487428+sunilkr123@users.noreply.github.com>
2020-10-23 16:21:55 +0530

    first commit

---
+++ CoreDataDemo/ListCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListCell.swift
+//  CoreDataDemo
+//
+//  Created by mac on 07/10/20.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+import UIKit
+
+class ListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
